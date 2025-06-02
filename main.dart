be6b9a439f49e3e28f88dd993662013b8708efc5
Herick <125935674+HerickYag@users.noreply.github.com>
2025-06-02 20:06:47 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:seu_projeto/screens/magos_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App de Magos',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MagosListScreen(),
+    );
+  }
+}
